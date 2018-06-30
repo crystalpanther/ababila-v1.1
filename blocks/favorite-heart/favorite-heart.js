81ef8ee6117b64c447ad5087f82ed526f220bbec
Elina <elina.lilova@gmail.com>
2018-06-30 13:15:09 +0300

    formed base directory folders, css styles

---
+++ blocks/favorite-heart/favorite-heart.js
@@ -0,0 +1,3 @@
+/**
+ * Created by elina on 6/29/2018.
+ */
---
+++ blocks/slider/slider.js
@@ -0,0 +1,13 @@
+/**
+ * Created by elina on 6/29/2018.
+ */
+$('.slider__gifts').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2
+});
+$('.slider__popular').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2
+});
